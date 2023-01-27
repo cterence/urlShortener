de2a6a273d30faad864e2e46fef9ca6e012d6389
Térence Chateigné <terencec@padok.fr>
2023-01-27 16:54:45 +0100

    update with devspace stuff

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 require("dotenv").config();
 
 const express = require("express");
+const winston = require("winston");
 
-const { createShortUrl, getUrl, getUrls, updateClicks, deleteUrl } = require("./models/url");
+const {
+    createShortUrl,
+    getUrl,
+    getUrls,
+    updateClicks,
+    deleteUrl,
+} = require("./models/url");
 
 const app = express();
+
+const logger = winston.createLogger({
+    format: winston.format.prettyPrint(),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({ filename: "combined.log" }),
+    ],
+});
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
@@ -16,28 +31,28 @@ app.get("/", async (req, res) => {
 
 app.post("/shortenUrl", async (req, res) => {
     if (req.body.fullUrl === null) return res.sendStatus(404);
-    await createShortUrl(req.body.fullUrl)
+    logger.info(`Shortening URL : ${req.body.fullUrl}`);
+    await createShortUrl(req.body.fullUrl);
     res.redirect("/");
 });
 
 app.get("/delete/:shortUrl", async (req, res) => {
     if (req.params.shortUrl === null) return res.sendStatus(404);
+    logger.info(`Delete short URL : ${req.params.shortUrl}`);
     await deleteUrl(req.params.shortUrl);
     res.redirect("/");
 });
 
-
 app.get("/:shortUrl", async (req, res) => {
-    const result = await getUrl(
-        req.params.shortUrl);
+    const result = await getUrl(req.params.shortUrl);
 
     if (!result.rows.length) return res.sendStatus(404);
 
     const shortUrl = result.rows[0];
     await updateClicks(parseInt(shortUrl.clicks) + 1, shortUrl.shorturl);
 
+    logger.info(`Redirecting ${shortUrl.shorturl} to ${shortUrl.fullurl}`);
     res.redirect(shortUrl.fullurl);
 });
-
 
 app.listen(3000);
